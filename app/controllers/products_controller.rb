d78f8b5c51bcc57a4aa4209706a7dadc6c6cd182
Cornellius Ngondo <devngondo@gmail.com>
2015-05-02 11:04:53 +0000

    Handling stock

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @product = Product.find_by_id params[:id]
+    # @product = Product.find(params[:id])
   end
 
   # GET /products/new
@@ -65,11 +65,11 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find params[:id]
+      @product = Product.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :price, :description, :image_url)
+      params.require(:product).permit(:title, :price, :description, :image_url, :stock)
     end
 end

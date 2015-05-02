28a46270398749eb43fd6c0d746b98660f6cdad3
Cornellius Ngondo <devngondo@gmail.com>
2015-05-02 12:26:43 +0000

    Managing orders before filter

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,7 +5,7 @@ module ProductsHelper
 
   def print_stock(stock)
     if stock > 0
-      content_tag(:td, "In stock (##)", class: ["in_stock"])
+      content_tag(:td, stock, class: ["in_stock"])
     else
       content_tag(:td, "Out of stock", class: ["out_stock"])
     end
